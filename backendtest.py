e1ac7a11fc648c0f9bc0ee9a1a7da03f734ba885
keemy <devin1891@gmail.com>
2013-11-17 05:34:32 -0800

    user personal counts

--- backendtest.py
+++ backendtest.py
@@ -7,19 +7,28 @@ import cgi
 import cgitb
 import sqlite3
 import os
+import Cookie
 cgitb.enable()
 conn = sqlite3.connect('test.db')
 c=conn.cursor()
 
 
-print os.environ["HTTP_COOKIE"]
+cookie= Cookie.SimpleCookie(os.environ["HTTP_COOKIE"])
+
+if "userName" not in cookie:
+    print "fuck u"
+    os.exit()
+
+userName=cookie["userName"].value
 
 form=cgi.FieldStorage()
 
-if "readonly" not in form:
-    c.execute("UPDATE counter SET num=num+1")
+c.execute("INSERT or IGNORE INTO counters VALUES (?,0)",tuple(userName))
 
-c.execute("SELECT * from counter")
+if "readonly" not in form:
+    c.execute("UPDATE counter SET count=count+1 WHERE userName=?",tuple(userName))
+
+c.execute("SELECT count FROM counters WHERE userName=?",tuple(userName))
 count=c.fetchone()
 print count[0]
 
