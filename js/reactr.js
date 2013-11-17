d55c69172461a1347668baa3be991c9cc7242867
keemy <devin1891@gmail.com>
2013-11-17 04:52:08 -0800

    user login :^)

--- js/reactr.js
+++ js/reactr.js
@@ -1,5 +1,15 @@
 var listOfTests=["sound","visual"];
 var userName=prompt("whats ur name");
+
+function setCookie(c_name,value,exdays){
+	var exdate=new Date();
+	exdate.setDate(exdate.getDate() + exdays);
+	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+	document.cookie=c_name + "=" + c_value;
+}
+
+setCookie("userName",userName,30);
+
 $( document ).ready(function() {
 	function IncrementAndReturn(){
 		$.ajax("backendtest.py").done(function(data){
